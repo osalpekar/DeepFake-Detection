2f6291db7a2484ece39c152a31886dffba096218
Omkar Salpekar <osalpekar@gmail.com>
2020-05-20 06:56:15 +0000

    some small changes to train on smaller data

--- data_preparation/image_extraction.py
+++ data_preparation/image_extraction.py
@@ -3,16 +3,16 @@ import json
 import cv2
 from glob import glob
 
-video_filenames = glob("/home/ubuntu/cs230_data/*/*.mp4")
+video_filenames = glob("/home/ubuntu/cs230_data/dfdc_train_part_0/*.mp4")
 num_examples = len(video_filenames) # change to use a smaller subset of the data
 subset_vid_names = video_filenames[:num_examples]
-meta_files = "/home/ubuntu/cs230_data/*/metadata.json"
+meta_files = ["/home/ubuntu/cs230_data/dfdc_train_part_0/metadata.json"]
 
 # TODO: make this a util and dedupe with equivalent function in the EDA folder
 metadata = {}
 for meta_file in meta_files:
     with open(meta_file) as f:
-	data = json.load(f)
+        data = json.load(f)
     metadata.update(data)
 
 labels = {}
@@ -23,7 +23,7 @@ for vid in subset_vid_names:
     name = name.split(".")[0] + ".jpg"
     labels[name] = label
 
-with open("/home/ubuntu/prep_data/labels.json", "w") as fp:
+with open("/home/ubuntu/prep_data/labels.json", "w+") as fp:
     json.dump(labels, fp)
 
 count = 0
@@ -36,16 +36,16 @@ for vid in subset_vid_names:
     name = os.path.basename(vid).split(".")[0]
     name = "/home/ubuntu/prep_data/" + name + ".jpg"
     cap = cv2.VideoCapture(vid)
-    i = 0
-    while cap.isOpened():
-        ret, frame = cap.read()
-        if ret == False:
-            break
-        if i % sampling_frequency == 0:
-            # we're only sampling once so we can retain the original name
-            cv2.imwrite(name, frame)
-        i += 1
+    #i = 0
+    #while cap.isOpened():
+    ret, frame = cap.read()
+    if ret == False:
+        break
+    #if i % sampling_frequency == 0:
+        # we're only sampling once so we can retain the original name
+    cv2.imwrite(name, frame)
+    #i += 1
     count += 1
     cap.release()
-    if count % 500 == 0:
+    if count % 50 == 0:
         print("images completed: " + str(count))
